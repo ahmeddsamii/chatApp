d255350acbb5a1d5939d1450d22371288ba05d58
ahmed samy <ahmbdelkhalek@gmail.com>
2025-02-04 15:55:18 +0200

    added dispatcher to the constructor

---
+++ app/src/main/java/com/example/mychatapp/presentation/ui/chat/ChatFragment.kt
@@ -0,0 +1,67 @@
+package com.example.mychatapp.presentation.ui.chat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.example.mychatapp.databinding.FragmentChatBinding
+import com.example.mychatapp.domain.entity.User
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ChatFragment : Fragment() {
+
+    private lateinit var args: ChatFragmentArgs
+    private lateinit var binding:FragmentChatBinding
+    private lateinit var user:User
+    private val chatViewMode:ChatViewModel by viewModels()
+    private val firebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentChatBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        args = ChatFragmentArgs.fromBundle(requireArguments())
+        user = args.user
+        setUpOpponentPersonData()
+
+
+        binding.sendBtn.setOnClickListener {
+            sendMessage()
+        }
+
+    }
+
+    private fun setUpOpponentPersonData(){
+        binding.chatUserName.text = user.username
+        binding.chatUserStatus.text = user.status
+        Glide.with(requireContext()).load(user.imageUrl).into(binding.chatImageViewUser)
+    }
+
+    private fun sendMessage(){
+        chatViewMode.sendMessage(
+            senderUid = firebaseAuth.currentUser!!.uid,
+            receiverUid = user.userId!!,
+            message = binding.editTextMessage.getText().toString(),
+            friendName = user.username!!,
+            friendImage = user.imageUrl!!,
+        )
+    }
+
+}
--- app/src/main/java/com/example/mychatapp/presentation/ui/signin/SignInViewModel.kt
+++ app/src/main/java/com/example/mychatapp/presentation/ui/signin/SignInViewModel.kt
@@ -3,9 +3,11 @@ package com.example.mychatapp.presentation.ui.signin
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.mychatapp.domain.usecase.SignInUseCase
+import com.example.mychatapp.presentation.di.IoDispatcher
 import com.example.mychatapp.utils.UIState
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
@@ -13,7 +15,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SignInViewModel @Inject constructor(
-    private val signInUseCase: SignInUseCase
+    private val signInUseCase: SignInUseCase,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
     private val _signInState = MutableStateFlow<UIState>(UIState.OnIdle)
@@ -31,13 +34,15 @@ class SignInViewModel @Inject constructor(
         if (!checkEmailAndPasswordAndUsername(email, password)){
             _signInState.value = UIState.OnFailure("Email and Password must be filled!")
         }else{
-            viewModelScope.launch {
+            viewModelScope.launch(dispatcher) {
                 signInUseCase.invoke(email, password).addOnCompleteListener{
-                    _signInState.value = UIState.OnSuccess(null)
+                    if (it.isSuccessful){
+                        _signInState.value = UIState.OnSuccess(null)
+                    }
                 }.addOnFailureListener{exception ->
                     when(exception){
                         is FirebaseAuthInvalidCredentialsException -> _signInState.value = UIState.OnFailure("Invalid Email or Password")
-                        else -> UIState.OnFailure("Something went wrong")
+                        else -> _signInState.value = UIState.OnFailure("Something went wrong") // This assigns the value
                     }
                 }
             }
