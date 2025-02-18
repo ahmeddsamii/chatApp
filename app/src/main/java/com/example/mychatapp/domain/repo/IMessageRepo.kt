7655b86890253d1c21dd576f6f49b0312b7505fc
ahmed samy <ahmbdelkhalek@gmail.com>
2025-02-18 10:19:05 +0200

    added fun of getMessages

--- app/src/main/java/com/example/mychatapp/domain/repo/IMessageRepo.kt
+++ app/src/main/java/com/example/mychatapp/domain/repo/IMessageRepo.kt
@@ -1,4 +1,8 @@
 package com.example.mychatapp.domain.repo
 
+import com.example.mychatapp.domain.entity.Message
+import kotlinx.coroutines.flow.Flow
+
 interface IMessageRepo {
+    suspend fun getMessages(friendUid:String): Flow<List<Message>>
 }
